00388c2df33eeafba37a0d304f4dfd8d94011e01
Rylan Zhou <oakenshield@MacBook-Pro.local>
2020-06-06 13:58:07 +0800

    Add tab bar navigation.

--- Meals App/navigator.js
+++ Meals App/navigator.js
@@ -1,12 +1,17 @@
+import React from 'react'
+import { Platform } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import { Platform } from 'react-native'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 import { COLORS } from './Constants'
 
 import {
   CategoriesScreen,
   CategoryMealsScreen,
-  MealDetailScreen
+  MealDetailScreen,
+  FavoritesScreen
 } from './Screens'
 
 const MealsNavigator = createStackNavigator(
@@ -35,4 +40,40 @@ const MealsNavigator = createStackNavigator(
   }
 )
 
-export default createAppContainer(MealsNavigator)
+const tabScreenConfig = {
+  Meals: {
+    // You can use a stack navigator as a screen
+    screen: MealsNavigator,
+    // This arg only matters when there is a nested navigator
+    navigationOptions: {
+      // Take tabInfo and return the icon
+      tabBarIcon: (tabInfo) => (
+        <Ionicons color={tabInfo.tintColor} name="ios-restaurant" size={25} />
+      )
+    }
+  },
+  Favorites: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarLabel: 'Favorites!', // Overwrite default tab label
+      tabBarIcon: (tabInfo) => (
+        <Ionicons color={tabInfo.tintColor} name="ios-star" size={25} />
+      )
+    }
+  }
+}
+
+const MealsFavTabNavigator =
+  // NOTE: Pay attention to the different configuration ways
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeColor: COLORS.secondary,
+        shifting: true // icons shift up when activated
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+          activeTintColor: COLORS.secondary
+        }
+      })
+
+export default createAppContainer(MealsFavTabNavigator)
